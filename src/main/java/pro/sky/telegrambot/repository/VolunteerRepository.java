52582f3bcae14b02ce131dcdba89f67f8bf83daa
Lyamzin <lyamzin01@mail.ru>
2024-07-21 20:52:32 +1000

    Сделал константы

--- src/main/java/pro/sky/telegrambot/repository/VolunteerRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/VolunteerRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.sky.telegrambot.entyty.Volunteer;
 
+import java.util.Optional;
 
 
 @Repository
@@ -11,5 +12,6 @@ public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {
 
     Volunteer findByNameVolunteer(String firstName);
 
-
+    Optional<Volunteer> findByChatId(long chatId);
+    Optional<Volunteer> findFirstByIsBusy(boolean bool);
 }
